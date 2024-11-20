4288b90c3734145107c7523b9b3588a837ffbfe2
Igor Markovsky <imbm2@yandex.ru>
2024-11-20 16:45:33 +0300

    Added homepage

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'homepage.apps.HomepageConfig',
     'catalog.apps.CatalogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
